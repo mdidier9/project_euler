d3880249a79898389327919859e6ec9192c02e1e
Matt Didier <mdidier9@gmail.com>
2014-06-11 16:04:08 -0500

    completed problem 1 - multiples of 3 or 5

--- code/Multiples_of_3_and_5.rb
+++ code/Multiples_of_3_and_5.rb
@@ -1,2 +1,14 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
+
+def multiples_of_3_or_5(max_num)
+	sum = 0
+	(1...max_num).each do |num|
+		if num % 3 == 0 || num % 5 == 0
+			sum += num
+		end
+	end
+	return sum
+end
+
+p multiples_of_3_or_5(1000) == 233168 #=> true
