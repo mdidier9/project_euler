36eee2af6390c282f367d66145ac9f1c721a0d32
Matt Didier <mdidier9@gmail.com>
2014-06-12 11:38:54 -0500

    completed solution to 3 - prime numbers)

--- code/3_largest_prime_factor.rb
+++ code/3_largest_prime_factor.rb
@@ -1,9 +1,17 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
-def largest_prime
-
-
+def largest_prime(num)
+	factor = 2
+	while factor < num do
+		if num % factor == 0
+			num = num/factor
+		else 
+			factor += 1
+		end
+	end
+	num
 end
 
-largest_prime(600851475143)
+p largest_prime(13195) == 29
+p largest_prime(600851475143) == 6857
